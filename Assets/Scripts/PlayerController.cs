161c04729db8b9d3bee79d7b29f658498628a486
Kaitlyn Chung <kachung2@s.sfusd.edu>
2023-02-03 13:18:51 -0800

    adjusting destroy game object script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +24,15 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange) {
             transform.position= new Vector3(xRange, transform.position.y, transform.position.z);
         }
+       
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    
     }
 }
