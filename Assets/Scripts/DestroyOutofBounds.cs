eb0e2260ec60c9c31f7de067ab4d9a828fdf21c0
Kaitlyn Chung <kachung2@s.sfusd.edu>
2023-02-10 12:06:31 -0800

    complete 2.4

--- Assets/Scripts/DestroyOutofBounds.cs
+++ Assets/Scripts/DestroyOutofBounds.cs
@@ -20,6 +20,7 @@ public class DestroyOutofBounds : MonoBehaviour
             Destroy(gameObject);
         } else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
         
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,15 +17,15 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (transform.position.x < -xRange) {
+    {if (transform.position.x < -xRange) {
             transform.position = new Vector3(-xRange, transform.position.y, transform. position.z);
         }
         if (transform.position.x > xRange) {
-            transform.position= new Vector3(xRange, transform.position.y, transform.position.z);
+          
+    transform.position= new Vector3(xRange, transform.position.y, transform.position.z);
         }
        
-        horizontalInput = Input.GetAxis("Horizontal");
+    horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
